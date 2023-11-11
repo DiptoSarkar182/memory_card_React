29f93943d44cc14c1f461ad197a980ea446cab2b
DiptoSarkar182 <diptomanchester2018@gmail.com>
2023-11-11 16:23:47 +0600

    only showing images

---
+++ memory_card/src/components/data.jsx
@@ -0,0 +1,12 @@
+import Aatrox from '../assets/Aatrox.webp';
+import Ahri from '../assets/Ahri_Render.webp';
+
+export const images = [{
+    id: 0,
+    name: 'Aatrox',
+    src: Aatrox,
+}, {
+    id: 1,
+    name: 'Ahri',
+    src: Ahri,
+}]
