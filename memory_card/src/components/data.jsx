dc26dce236fd6fcdfe5851082224182925558eb2
DiptoSarkar182 <diptomanchester2018@gmail.com>
2023-11-18 13:13:12 +0600

    code formatted

--- memory_card/src/components/data.jsx
+++ memory_card/src/components/data.jsx
@@ -1,155 +1,184 @@
-import Aatrox from '../assets/Aatrox.webp';
-import Ahri from '../assets/Ahri_Render.webp';
-import Akali from '../assets/Akali_Render.webp';
-import Akshan from '../assets/Akshan_Render.webp';
-import Alister from '../assets/Alistar_Render.webp';
-import Bard from '../assets/Bard_Render.webp';
-import BelVeth from '../assets/BelVeth_Render.webp';
-import Blitzcrank from '../assets/Blitzcrank_Render.webp';
-import Brand from '../assets/Brand_Render.webp';
-import Braum from '../assets/Braum_Render.webp';
-import Caitlyn from '../assets/Caitlyn_Render.webp';
-import Camile from '../assets/Camille_Render.webp';
-import Cassiopeia from '../assets/Cassiopeia_Render.webp';
-import ChoGath from '../assets/ChoGath_Render.webp';
-import Corki from '../assets/Corki_Render.webp';
-import Darius from '../assets/Darius_Render.webp';
-import Diana from '../assets/Diana_Render.webp';
-import Dr_Mundo from '../assets/Dr._Mundo_Render.webp';
-import Draven from '../assets/Draven_Render.webp';
-import Ekko from '../assets/Ekko_Render.webp';
-import Fiora from '../assets/Fiora_Render.webp';
-import Fizz from '../assets/Fizz_Render.webp';
-import Galio from '../assets/Galio_Render.webp';
-import GangPlank from '../assets/Gangplank_Render.webp';
-import Garen from '../assets/Garen_Render.webp';
-import Hecarim from '../assets/Hecarim_Render.webp';
-import Heimerdinger from '../assets/Heimerdinger_Render.webp';
-import Illaoi from '../assets/Illaoi_Render.webp';
-import Irelia from '../assets/Irelia_Render.webp';
-import Ivern from '../assets/Ivern_Render.webp';
-import { v4 as uuid } from 'uuid';
+import Aatrox from "../assets/Aatrox.webp";
+import Ahri from "../assets/Ahri_Render.webp";
+import Akali from "../assets/Akali_Render.webp";
+import Akshan from "../assets/Akshan_Render.webp";
+import Alister from "../assets/Alistar_Render.webp";
+import Bard from "../assets/Bard_Render.webp";
+import BelVeth from "../assets/BelVeth_Render.webp";
+import Blitzcrank from "../assets/Blitzcrank_Render.webp";
+import Brand from "../assets/Brand_Render.webp";
+import Braum from "../assets/Braum_Render.webp";
+import Caitlyn from "../assets/Caitlyn_Render.webp";
+import Camile from "../assets/Camille_Render.webp";
+import Cassiopeia from "../assets/Cassiopeia_Render.webp";
+import ChoGath from "../assets/ChoGath_Render.webp";
+import Corki from "../assets/Corki_Render.webp";
+import Darius from "../assets/Darius_Render.webp";
+import Diana from "../assets/Diana_Render.webp";
+import Dr_Mundo from "../assets/Dr._Mundo_Render.webp";
+import Draven from "../assets/Draven_Render.webp";
+import Ekko from "../assets/Ekko_Render.webp";
+import Fiora from "../assets/Fiora_Render.webp";
+import Fizz from "../assets/Fizz_Render.webp";
+import Galio from "../assets/Galio_Render.webp";
+import GangPlank from "../assets/Gangplank_Render.webp";
+import Garen from "../assets/Garen_Render.webp";
+import Hecarim from "../assets/Hecarim_Render.webp";
+import Heimerdinger from "../assets/Heimerdinger_Render.webp";
+import Illaoi from "../assets/Illaoi_Render.webp";
+import Irelia from "../assets/Irelia_Render.webp";
+import Ivern from "../assets/Ivern_Render.webp";
+import { v4 as uuid } from "uuid";
 
-
-export const images = [{
+export const images = [
+  {
     id: uuid(),
-    name: 'Aatrox',
-    src: Aatrox
-}, {
+    name: "Aatrox",
+    src: Aatrox,
+  },
+  {
     id: uuid(),
-    name: 'Ahri',
-    src: Ahri
-}, {
+    name: "Ahri",
+    src: Ahri,
+  },
+  {
     id: uuid(),
-    name: 'Akali',
-    src: Akali
-}, {
+    name: "Akali",
+    src: Akali,
+  },
+  {
     id: uuid(),
-    name: 'Akshan',
-    src: Akshan
-}, {
+    name: "Akshan",
+    src: Akshan,
+  },
+  {
     id: uuid(),
-    name: 'Alister',
-    src: Alister
-}, {
+    name: "Alister",
+    src: Alister,
+  },
+  {
     id: uuid(),
-    name: 'Bard',
-    src: Bard
-}, {
+    name: "Bard",
+    src: Bard,
+  },
+  {
     id: uuid(),
-    name: 'BelVeth',
-    src: BelVeth
-}, {
+    name: "BelVeth",
+    src: BelVeth,
+  },
+  {
     id: uuid(),
-    name: 'Blitzcrank',
-    src: Blitzcrank
-}, {
+    name: "Blitzcrank",
+    src: Blitzcrank,
+  },
+  {
     id: uuid(),
-    name: 'Brand',
-    src: Brand
-}, {
+    name: "Brand",
+    src: Brand,
+  },
+  {
     id: uuid(),
-    name: 'Braum',
-    src: Braum
-}, {
+    name: "Braum",
+    src: Braum,
+  },
+  {
     id: uuid(),
-    name: 'Caitlyn',
-    src: Caitlyn
-}, {
+    name: "Caitlyn",
+    src: Caitlyn,
+  },
+  {
     id: uuid(),
-    name: 'Camille',
-    src: Camile
-}, {
+    name: "Camille",
+    src: Camile,
+  },
+  {
     id: uuid(),
-    name: 'Cassiopeia',
-    src: Cassiopeia
-}, {
+    name: "Cassiopeia",
+    src: Cassiopeia,
+  },
+  {
     id: uuid(),
-    name: 'ChoGath',
-    src: ChoGath
-}, {
+    name: "ChoGath",
+    src: ChoGath,
+  },
+  {
     id: uuid(),
-    name: 'Corki',
-    src: Corki
-}, {
+    name: "Corki",
+    src: Corki,
+  },
+  {
     id: uuid(),
-    name: 'Darius',
-    src: Darius
-}, {
+    name: "Darius",
+    src: Darius,
+  },
+  {
     id: uuid(),
-    name: 'Diana',
-    src: Diana
-}, {
+    name: "Diana",
+    src: Diana,
+  },
+  {
     id: uuid(),
-    name: 'Dr. Mundo',
-    src: Dr_Mundo
-}, {
+    name: "Dr. Mundo",
+    src: Dr_Mundo,
+  },
+  {
     id: uuid(),
-    name: 'Draven',
-    src: Draven
-}, {
+    name: "Draven",
+    src: Draven,
+  },
+  {
     id: uuid(),
-    name: 'Ekko',
-    src: Ekko
-}, {
+    name: "Ekko",
+    src: Ekko,
+  },
+  {
     id: uuid(),
-    name: 'Fiora',
-    src: Fiora
-}, {
+    name: "Fiora",
+    src: Fiora,
+  },
+  {
     id: uuid(),
-    name: 'Fizz',
-    src: Fizz
-}, {
+    name: "Fizz",
+    src: Fizz,
+  },
+  {
     id: uuid(),
-    name: 'Galio',
-    src: Galio
-}, {
+    name: "Galio",
+    src: Galio,
+  },
+  {
     id: uuid(),
-    name: 'Gangplank',
-    src: GangPlank
-}, {
+    name: "Gangplank",
+    src: GangPlank,
+  },
+  {
     id: uuid(),
-    name: 'Garen',
-    src: Garen
-}, {
+    name: "Garen",
+    src: Garen,
+  },
+  {
     id: uuid(),
-    name: 'Hecarim',
-    src: Hecarim
-}, {
+    name: "Hecarim",
+    src: Hecarim,
+  },
+  {
     id: uuid(),
-    name: 'Heimerdinger',
-    src: Heimerdinger
-}, {
+    name: "Heimerdinger",
+    src: Heimerdinger,
+  },
+  {
     id: uuid(),
-    name: 'Illaoi',
-    src: Illaoi
-}, {
+    name: "Illaoi",
+    src: Illaoi,
+  },
+  {
     id: uuid(),
-    name: 'Irelia',
-    src: Irelia
-}, {
+    name: "Irelia",
+    src: Irelia,
+  },
+  {
     id: uuid(),
-    name: 'Ivern',
-    src: Ivern
-}
+    name: "Ivern",
+    src: Ivern,
+  },
 ];
